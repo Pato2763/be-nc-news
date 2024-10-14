a1226dcde73f4035bab7e57ecdfa36e972e7265f
Pato2763 <daviespatrick2763@gmail.com>
2024-10-14 14:03:08 +0100

    completed task 3

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,6 +8,7 @@ const {
   topicData,
   userData,
 } = require("../db/data/test-data/index.js");
+const fs = require("node:fs/promises");
 
 beforeEach(() => seed({ topicData, userData, articleData, commentData }));
 afterAll(() => db.end());
@@ -34,6 +35,24 @@ describe("GET /api/topics", () => {
           expect(typeof topic.slug).toBe("string");
           expect(typeof topic.description).toBe("string");
         });
+      });
+  });
+});
+
+describe("GET /api", () => {
+  test("GET 200 sends an array of all the valid endpoints", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body }) => {
+        const endpoints = fs.readFile("endpoints.json", "utf-8");
+        return Promise.all([body, endpoints]);
+      })
+      .then(([body, endpoints]) => {
+        console.log(body);
+        console.log(endpoints);
+        const parsedEndpoints = JSON.parse(endpoints);
+        expect(body).toEqual({ endpoints: parsedEndpoints });
       });
   });
 });
