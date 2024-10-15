8cd40b506692a44f95918c2ae4a404ae319ff0a7
Pato2763 <daviespatrick2763@gmail.com>
2024-10-15 14:07:34 +0100

    removed commented out code

--- models/articles-model.js
+++ models/articles-model.js
@@ -17,27 +17,6 @@ exports.fetchArticlesbyId = (id) => {
       return rows;
     });
 };
-// exports.fetchArticles = () => {
-//   return db
-//     .query(
-//       `SELECT author,title,article_id,topic,created_at,votes,article_img_url FROM articles
-//       ORDER BY created_at DESC`
-//     )
-//     .then(({ rows }) => {
-//       const articles = rows;
-//       const query = `SELECT * FROM comments`;
-//       return Promise.all([articles, db.query(query)]);
-//     })
-//     .then(([articles, response]) => {
-//       const comments = response.rows;
-//       return articles.map((article) => {
-//         article.comment_count = comments.filter((comment) => {
-//           return comment.article_id === article.article_id;
-//         }).length;
-//         return article;
-//       });
-//     });
-// };
 
 exports.fetchArticles = () => {
   return db
